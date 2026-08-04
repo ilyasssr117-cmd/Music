302f7ecac535b7dfb8685a0461a3984f126f1dbe
ilyasssr117-cmd <ilyasssr117@gmail.com>
2026-08-05 00:33:08 +0100

    Delete lib directory

--- lib/services/app_navigation_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppNavigationService {
-  static final GlobalKey<NavigatorState> rootNavigatorKey =
-      GlobalKey<NavigatorState>();
-
-  const AppNavigationService._();
-}
